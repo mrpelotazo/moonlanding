74e0663c868bde4226dcafee869f3841e766169b
Adrian Antonana <a.antonana@gmail.com>
2013-06-29 09:26:36 +0200

    ship.cpp: Added the three flame sprites.

--- ship.cpp
+++ ship.cpp
@@ -4,81 +4,74 @@
 CShip::CShip (short int initposx, short int initposy) {
 
 	/* rectangles init */
-	shiprect = new SDL_Rect {0, 0, SHIP_SHAPE_W, SHIP_SHAPE_H};
-	flamerect = new SDL_Rect {0, SHIP_SHAPE_H-FLAME_OVERLAP, FLAME_SHAPE_W, FLAME_SHAPE_H};
-	rect = new SDL_Rect {initposx, initposy, SHIP_SHAPE_W, SHIP_SHAPE_H + FLAME_SHAPE_H};
-	oldrect = new SDL_Rect {initposx, initposy, SHIP_SHAPE_W, SHIP_SHAPE_H};
+	shiprect = new SDL_Rect {initposx, initposy, SHIP_SHAPE_W, SHIP_SHAPE_H};
+	shiprectclr = new SDL_Rect {0, 0, SHIP_SHAPE_W + 10, SHIP_SHAPE_H + 10};
+	flamerect = new SDL_Rect {initposx, (short int)(initposy+FLAME_SHAPE_H), FLAME_SHAPE_W, FLAME_SHAPE_H};
 
-	/* ship surface init */
-	shipsurface = SDL_CreateRGBSurface(0, shiprect->w, shiprect->h, 32, 0, 0, 0, 0);
-	SDL_FillRect(shipsurface, NULL, SDL_MapRGBA(shipsurface->format, 0, 0, 1, 0));
-	aapolygonRGBA(shipsurface, shapevertx, shapeverty, 4, 255, 255, 255, 255);
+	/* load the ship sprite */
+	shipsprite = IMG_Load(SHIP_SPRITE);
 
-	/* flame surface under the ship init */
-	flamesurface = SDL_CreateRGBSurface(0, flamerect->w, flamerect->h, 32, 0, 0, 0, 0);
-	SDL_FillRect(flamesurface, NULL, SDL_MapRGBA(flamesurface->format, 0, 0, 1, 0));
-	aatrigonRGBA(flamesurface, 5, 0, flamerect->w-5, 0, flamerect->w / 2, flamerect->h, 255,0,0,255);
-
-	/* the final surface will contain shipsurface + flamesurface */
-	surface = SDL_CreateRGBSurface(0, rect->w, rect->h, 32, 0, 0, 0, 0);
-	SDL_FillRect(surface, NULL, SDL_MapRGBA(surface->format, 0, 0, 1, 0));
-
-	/* surface for clearing the final surface (is a little bit bigger) */
-	clearsurface = SDL_CreateRGBSurface(0, rect->w + CLEAR_SURFACE_OFFSET, rect->h + CLEAR_SURFACE_OFFSET, 32, 0, 0, 0, 0);
-	SDL_FillRect(clearsurface, NULL, SDL_MapRGBA(clearsurface->format, 0, 0, 1, 0));
+	/* load the flame sprites */
+	flamespritebig = IMG_Load(FLAME_SPRITE_BIG);
+	flamespritemid = IMG_Load(FLAME_SPRITE_MID);
+	flamespritelow = IMG_Load(FLAME_SPRITE_LOW);
 
 	/* eagle init */
 	eagle = new CEagle ((float)initposx, (float)initposy);
-}
 
-CShip::~CShip() {
-//	SDL_FreeSurface(shipsurface);
-//	SDL_FreeSurface(flamesurface);
-//	SDL_FreeSurface(surface);
-//	SDL_FreeSurface(rotsurface);
-//	SDL_FreeSurface(clearsurface);
-//	SDL_FreeSurface(rotclearsurface);
+	/* flame off*/
+	drawflame = 0;
 }
 
 void CShip::update (unsigned int timedelta) {
 
-	/* save the old position for clearing the surface */
-	oldrect->x = rect->x;
-	oldrect->y = rect->y;
-
-	/* offset the oldrect to avoid trailing pixels */
-	/* this will affect the blitting of the clearing surface */
-	oldrect->x -= CLEAR_SURFACE_OFFSET / 2;
-	oldrect->y -= CLEAR_SURFACE_OFFSET / 2;
-
-	/* rotate clear surface before with old angle */
-	rotclearsurface = rotozoomSurface(clearsurface, -eagle->getRawAngDeg(), 1.0, 0);
-
 	/* update eagle data */
 	eagle->update(timedelta);
-	drawflame = eagle->getMainEngineStatus();
+	
+	/* apply new position to ship and flame rect */
+	shiprect->x = (unsigned short)eagle->getPosx();
+	shiprect->y = -(unsigned short)eagle->getPosy();
+	flamerect->x = (unsigned short)eagle->getPosx();
+	flamerect->y = -(unsigned short)eagle->getPosy()+FLAME_SHAPE_H;
 
-	/* reblit the total surface */
-	SDL_BlitSurface(clearsurface, NULL, surface, NULL);
-    if (drawflame) {
-		SDL_BlitSurface(flamesurface, NULL, surface, flamerect);
+
+	/* apply new rotation to ship surface */
+	shiprotsprite = rotozoomSurface(shipsprite, -eagle->getRawAngDeg(), 1.0, 0);
+
+	/* apply rotation to flame surface */
+	switch (eagle->getMainEngineStatus()) {
+		case MAX_MAIN_THRUST:
+			flamerotsprite = rotozoomSurface(flamespritebig, -eagle->getRawAngDeg(), 1.0, 0);
+			flamerect->x -= flamerotsprite->w/2 - flamespritebig->w/2;
+			flamerect->y -= flamerotsprite->h/2 - flamespritebig->h/2;
+			break;
+		case MID_MAIN_THRUST:
+			flamerotsprite = rotozoomSurface(flamespritemid, -eagle->getRawAngDeg(), 1.0, 0);
+			flamerect->x -= flamerotsprite->w/2 - flamespritemid->w/2;
+			flamerect->y -= flamerotsprite->h/2 - flamespritemid->h/2;
+			break;
+		case LOW_MAIN_THRUST:
+			flamerotsprite = rotozoomSurface(flamespritelow, -eagle->getRawAngDeg(), 1.0, 0);
+			flamerect->x -= flamerotsprite->w/2 - flamespritelow->w/2;
+			flamerect->y -= flamerotsprite->h/2 - flamespritelow->h/2;
+			break;
+		default:
+			break;
 	}
-	SDL_BlitSurface(shipsurface, NULL, surface, shiprect);
 
-	/* apply new position to total surface */
-	rect->x = (unsigned short)eagle->getPosx();
-	rect->y = -(unsigned short)eagle->getPosy();
-
-
-	/* apply new rotation to the total surface */
-	rotsurface = rotozoomSurface(surface, -eagle->getRawAngDeg(), 1.0, 0);
-
-	/* make the surface rotate around its center */
-	rect->x -= rotsurface->w/2 - surface->w/2;
-	rect->y -= rotsurface->h/2 - surface->h/2;
+	/* make the surfaces rotate around its center */
+	shiprect->x -= shiprotsprite->w/2 - shipsprite->w/2;
+	shiprect->y -= shiprotsprite->h/2 - shipsprite->h/2;
 }
 
-void CShip::draw (SDL_Surface *dstsurface) {
-	SDL_BlitSurface(rotclearsurface, NULL, dstsurface, oldrect);
-	SDL_BlitSurface(rotsurface, NULL, dstsurface, rect);
+void CShip::clear(SDL_Surface *dst) {
+	SDL_FillRect(dst, shiprect, SDL_MapRGB(dst->format, 0, 0, 0));
+	SDL_FillRect(dst, flamerect, SDL_MapRGB(dst->format, 0, 0, 0));
+}
+
+void CShip::draw (SDL_Surface *dst) {
+	if (drawflame = eagle->getMainEngineStatus()) {
+		SDL_BlitSurface(flamerotsprite, NULL, dst, flamerect);
+	}
+	SDL_BlitSurface(shiprotsprite, NULL, dst, shiprect);
 }
