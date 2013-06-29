78085b67ad13ab4ff1904b5550bc9084254ca705
Adrian Antonana <a.antonana@gmail.com>
2013-06-29 09:27:56 +0200

    moonlanding.hpp: Added stars background and ground functions

--- moonlanding.hpp
+++ moonlanding.hpp
@@ -11,7 +11,11 @@
 #define SCREEN_H (SHIP_SURFACE_H + INFO_SURFACE_H)
 #define STAR_NUM 500
 #define STAR_COLOR 0x606060
-
+#define MAX_PEAKS_PER_SIDE 5
+#define MAX_PEAKS MAX_PEAKS_PER_SIDE * 2 + 10 
+#define MAX_GRND_H SCREEN_H/2
+#define MIN_GRND_H SCREEN_H/2
+#define LAND_POS_W SHIP_SHAPE_W + 20
 #define FONT_PATH "/usr/share/fonts/TTF/DejaVuSansMono.ttf"
 #define FONT_SIZE 17
 
@@ -32,6 +36,18 @@ int clearSurface(SDL_Surface *ps);
 
 /* the event handler */
 void eventHandler(SDL_Event e, CShip s, unsigned short *r);
+
+/* generate stars */
+void genStars(int slx[], int sly[]);
+
+/* draw stars */
+void drawStars(SDL_Surface *dst, int slx[], int sly[]);
+
+/* generate ground */
+void genGround(int glx[], int gly[], int *length);
+
+/* draw ground */
+void drawGround(SDL_Surface *dst, int glx[], int gly[], int length);
 
 /* Shows the eagle information on the given surface */
 void showShipInfo(SDL_Surface *ps, CShip s, TTF_Font *pf, SDL_Color c);
