f8e0754a70ee4de56015485e29ff49f02ca72e36
Adrian Antonana <a.antonana@gmail.com>
2013-06-29 09:28:30 +0200

    moonlanding.cpp: Added stars background and ground functions

--- moonlanding.cpp
+++ moonlanding.cpp
@@ -20,30 +20,97 @@ SDL_Surface *initSurface(SDL_Rect r) {
 	return SDL_CreateRGBSurface(SDL_HWSURFACE, r.w, r.h, 32, 0, 0, 0, 0);
 }
 
-void drawStars(SDL_Surface *s) {
-	int posx, posy;
-	int starposxlist[STAR_NUM];
-	int starposylist[STAR_NUM];
+void genStars(int slx[], int sly[]) {
 	time_t timer;
 
 	srand ((unsigned int)time(&timer));
     
 	for (int i=0; i<STAR_NUM; i++) {
-		starposxlist[i] = rand() % s->w;
-		starposylist[i] = rand() % s->h;
+		slx[i] = rand() % SCREEN_W;
+		sly[i] = rand() % SCREEN_H;
 	}
+}
+
+void drawStars(SDL_Surface *s, int slx[], int sly[]) {
+	int posx, posy;
 
 	SDL_LockSurface(s);
 
 	for (int i=0; i<STAR_NUM; i++) {
-		posx = starposxlist[i];
-		posy = starposylist[i];
+		posx = slx[i];
+		posy = sly[i];
 		int bpp = s->format->BytesPerPixel;
 		Uint8 *p = (Uint8 *)s->pixels + posy * s->pitch + posx * bpp;
 		*(Uint32 *)p = STAR_COLOR;
 	}
 
 	SDL_UnlockSurface(s);
+}
+
+void genGround(int glx[], int gly[], int *length) {
+	/* generate the x list */
+	int last_x = 0;
+	int x_aux;
+	int pos = 0;
+
+	srand(time(NULL));
+
+	glx[pos] = 0;
+	pos++;
+
+	while ((last_x < (SCREEN_W / 2) - 1) && (pos < MAX_PEAKS_PER_SIDE)) {
+		x_aux = rand() % (SCREEN_W / 2);
+
+		while (x_aux <= last_x) {
+			x_aux = rand() % (SCREEN_W / 2);
+		}
+
+		glx[pos] = x_aux;
+		last_x = x_aux;
+		pos++;
+	}
+
+	glx[pos] = last_x + LAND_POS_W;
+	int land_posx_in_list = pos - 1;
+	last_x = last_x + LAND_POS_W;
+	pos++;
+
+	while ((last_x < SCREEN_W - 1) && (pos < MAX_PEAKS_PER_SIDE * 2 + 2)) {
+		x_aux = rand() % (SCREEN_W);
+
+		while (x_aux <= last_x) {
+			x_aux = rand() % (SCREEN_W);
+		}
+
+		glx[pos] = x_aux;
+		last_x = x_aux;
+		pos++;
+	}
+
+	glx[pos] = SCREEN_W;
+	pos++;
+	int x_elems_num = pos;
+	/* generate the y list
+	 * the number of peak elements depends on the previously generated x
+	 * position elements */
+	for (int i=0; i<=land_posx_in_list; i++) {
+		gly[i] = (rand() % MIN_GRND_H) + MAX_GRND_H;
+	}
+
+	gly[land_posx_in_list + 1] = gly[land_posx_in_list];
+
+	for (int i=land_posx_in_list + 2 ; i<x_elems_num; i++) {
+		gly[i] = (rand() % MIN_GRND_H) + MAX_GRND_H;
+	}
+
+	/* return the list length */
+	*length = x_elems_num;
+}
+
+void drawGround(SDL_Surface *dst, int glx[], int gly[], int length) {
+	for (int i=0; i<length-2; i++) {
+		lineRGBA(dst, glx[i], gly[i], glx[i+1], gly[i+1],255,255,255,255);
+	}
 }
 
 int clearSurface(SDL_Surface *ps) {
